32a83b8db925ad7b8bd28f2787e8927d288bd2cc
César Gabriel Leañez <93480406+gabriedev@users.noreply.github.com>
2023-02-24 13:01:04 -0400

    Update vite.config.js [Conditional Config]

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  build: { outDir: 'build' }
+export default defineConfig(({ mode }) => {
+  base: mode === 'build' ? '/notes-app/' : '/',
+  build: {
+    outDir: 'build',
+  },
 })
